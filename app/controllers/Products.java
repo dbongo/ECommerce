71dcaf1d22c0242e99191ca72523b31bd5067f47
Oscar Rundh <mr.rundh@gmail.com>
2014-03-12 14:29:36 +0100

    Updates product names to something more human

--- app/controllers/Products.java
+++ app/controllers/Products.java
@@ -16,10 +16,10 @@ import views.html.*;
 public class Products extends Controller {
 
 	private static Map<String, Product> productRepo = new HashMap<String, Product>(){{
-		put("1", new Product(1, "test", "testerer", 100, new LinkedList<Category>(Arrays.asList(new Category(1, "cat1")))));	
-		put("2", new Product(2, "test2", "testerer2", 210, new LinkedList<Category>(Arrays.asList(new Category(2, "cat2")))));	
-		put("3", new Product(3, "test3", "testerer3", 320, new LinkedList<Category>(Arrays.asList(new Category(1, "cat1"),new Category(2, "cat2")))));	
-		put("4", new Product(4, "test4", "testerer4", 430, new LinkedList<Category>(Arrays.asList(new Category(1, "cat1"), new Category(2, "cat2")))));	
+		put("1", new Product(1, "Hobbit", "There and back again", 100, new LinkedList<Category>(Arrays.asList(new Category(1, "Book"), new Category(1, "Movie")))));	
+		put("2", new Product(2, "The Bro Code", "This book is aaaaawesome!", 210, new LinkedList<Category>(Arrays.asList(new Category(2, "Book")))));	
+		put("3", new Product(3, "Iron Man 3", "Tinman goes wild", 320, new LinkedList<Category>(Arrays.asList(new Category(1, "Movie")))));	
+		put("4", new Product(4, "Thor 3", "By the power of Zues!", 430, new LinkedList<Category>(Arrays.asList(new Category(1, "Movie")))));	
 	}};
 
     public static Result allProducts() {
