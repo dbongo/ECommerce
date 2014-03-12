6b1871aa3e21d439e098cf1d79121ceb4ef769e8
Oscar Rundh <mr.rundh@gmail.com>
2014-03-12 11:29:19 +0100

    index.scala.html will be a base template
    index.scala.html will be a template to serve the other views in.
    The path / will point to startpage instead of directly to index.
    
    / !-> index
    / -> startpage

--- app/controllers/Categories.java
+++ app/controllers/Categories.java
@@ -8,10 +8,10 @@ import views.html.*;
 public class Categories extends Controller {
 
     public static Result allCategories() {
-        return ok(index.render("Look at this beautiful list of invisible categories"));
+        return ok(categories.render("Look at this beautiful list of invisible categories"));
     }
 
     public static Result category(String id) {
-        return ok(index.render("Displaying category with id: " + id));
+        return ok(category.render("Displaying category with id: " + id));
     }
 }
