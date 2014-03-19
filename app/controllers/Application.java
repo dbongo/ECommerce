44c4c830e242b30ee18c74422f25ca6caf338cb9
Oscar Rundh <mr.rundh@gmail.com>
2014-03-19 11:29:21 +0100

    Now auto-populates the tables with data

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -16,37 +16,36 @@ public class Application extends Controller {
 	
 	static boolean productsAdded = false;
 	
-	@Transactional
-	public static void createDB() {
-		
-		if (!productsAdded) {
-			JPA.em().persist(new Category("Book", "fa-book"));
-			JPA.em().persist(new Category("Movie", "fa-film"));
-			JPA.em().persist(new Category("TV-Show", "fa-desktop"));
-			
-			JPA.em().persist(new Product("Hobbit", "There and back again", "hobbit.jpg", 100, new LinkedList<Category>(Arrays.asList( JPA.em().find(Category.class, 1),  JPA.em().find(Category.class, 2)))));	
-			JPA.em().persist(new Product("The Bro Code", "This book is aaaaawesome!", "bro-code.jpg", 210, new LinkedList<Category>(Arrays.asList( JPA.em().find(Category.class, 1)))));	
-			JPA.em().persist(new Product("Iron Man 3", "Tinman goes wild", "iron-man-3.jpg", 320, new LinkedList<Category>(Arrays.asList( JPA.em().find(Category.class, 2)))));	
-			JPA.em().persist(new Product("Thor 2", "By the power of Zues!", "thor-2.jpg", 430, new LinkedList<Category>(Arrays.asList( JPA.em().find(Category.class, 2)))));
-			JPA.em().persist(new Product("True Blood", "It sucks", "true-blood.jpg", 420, new LinkedList<Category>(Arrays.asList( JPA.em().find(Category.class, 1),  JPA.em().find(Category.class, 3)))));
-			JPA.em().persist(new Product("Doctor Who", "Wibbly wobbly timey wimey", "doctor-who.jpg", 430, new LinkedList<Category>(Arrays.asList( JPA.em().find(Category.class, 3)))));
-			JPA.em().persist(new Product("Game of Thrones", "All men must die!", "song-of-ice-and-fire.jpg", 430, new LinkedList<Category>(Arrays.asList( JPA.em().find(Category.class, 1),  JPA.em().find(Category.class, 3)))));
-			productsAdded = true;
-		}
-	}
+//	@Transactional
+//	public static void createDB() {
+//		if (!productsAdded) {
+//			JPA.em().persist(new Category("Book", "fa-book"));
+//			JPA.em().persist(new Category("Movie", "fa-film"));
+//			JPA.em().persist(new Category("TV-Show", "fa-desktop"));
+//			
+//			JPA.em().persist(new Product("Hobbit", "There and back again", "hobbit.jpg", 100, new LinkedList<Category>(Arrays.asList( JPA.em().find(Category.class, 1),  JPA.em().find(Category.class, 2)))));	
+//			JPA.em().persist(new Product("The Bro Code", "This book is aaaaawesome!", "bro-code.jpg", 210, new LinkedList<Category>(Arrays.asList( JPA.em().find(Category.class, 1)))));	
+//			JPA.em().persist(new Product("Iron Man 3", "Tinman goes wild", "iron-man-3.jpg", 320, new LinkedList<Category>(Arrays.asList( JPA.em().find(Category.class, 2)))));	
+//			JPA.em().persist(new Product("Thor 2", "By the power of Zues!", "thor-2.jpg", 430, new LinkedList<Category>(Arrays.asList( JPA.em().find(Category.class, 2)))));
+//			JPA.em().persist(new Product("True Blood", "It sucks", "true-blood.jpg", 420, new LinkedList<Category>(Arrays.asList( JPA.em().find(Category.class, 1),  JPA.em().find(Category.class, 3)))));
+//			JPA.em().persist(new Product("Doctor Who", "Wibbly wobbly timey wimey", "doctor-who.jpg", 430, new LinkedList<Category>(Arrays.asList( JPA.em().find(Category.class, 3)))));
+//			JPA.em().persist(new Product("Game of Thrones", "All men must die!", "song-of-ice-and-fire.jpg", 430, new LinkedList<Category>(Arrays.asList( JPA.em().find(Category.class, 1),  JPA.em().find(Category.class, 3)))));
+//			productsAdded = true;
+//		}
+//	}
 	
 	public static Result index() {
-		if (productsAdded) {
+//		if (productsAdded) {
 			return ok(startpage.render("<p>Happy browsing!</p>"));
-		} else {
-			return ok(startpage.render("<p><a href='/setup'>Click me</a> to populate this site with content!</p>"));	
-		}
+//		} else {
+//			return ok(startpage.render("<p><a href='/setup'>Click me</a> to populate this site with content!</p>"));	
+//		}
 		
 	}
 	
-	@Transactional
-	public static Result setup() {
-		createDB();
-		return redirect("/");	
-	}
+//	@Transactional
+//	public static Result setup() {
+//		createDB();
+//		return redirect("/");	
+//	}
 }
